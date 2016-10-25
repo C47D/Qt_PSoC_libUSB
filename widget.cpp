f5769f4fccfcde6a33ee118234bb18a390436bc9
C47D <carlos.santiago.diaz@gmail.com>
2016-10-24 19:15:13 -0500

    Comments cleanup

--- widget.cpp
+++ widget.cpp
@@ -16,51 +16,39 @@ Widget::Widget(QWidget *parent) :
 {
     ui->setupUi(this);
 
-    /***** QTimer Setuo *****/
+    /***** QTimer Setup *****/
     timer = new QTimer(this);
 
     /********** Setup GUI visual contents **********/
 
-    /*
-     * buttonState Check Button:
-     * this check button let us know the state
-     * of the onboard switch button of the
-     * development kit.
-    */
+    // buttonState Check Button:
+    // this check button let us know the state
+    // of the onboard switch button of the
+    // development kit.
     ui->buttonState_cB->setText("Push button state");
 
-    /*
-     * scrollBar Label
-     * This is just a label to
-     * display a text, nothing fancy here
-    */
+    // scrollBar Label
+    // This is just a label to
+    // display a text, nothing fancy here
     ui->scrollBar_lbl->setText("PrISM Density Value");
 
-    /*
-     * connect Push Button
-     * This button will allow us to
-     * connect and disconnect the USB device to
-     * the application.
-     * Acquire and release in libUSB terms, it will
-     * also display a text, which will be the state
-     * of the device (connected or disconnected) being
-     * the last state the default state
-    */
+    // connect Push Button
+    // This button will allow us to
+    // connect and disconnect the USB device to
+    // the application.
+    // Acquire and release in libUSB terms, it will
+    // also display a text, which will be the state
+    // of the device (connected or disconnected) being
+    // the last state the default state
     ui->connect_pB->setText("Connect");
     ui->connect_pB->setStyleSheet("QPushButton {font-weight: bold; color : green}");
 
-    /*
-     *
-     *
-    */
     ui->state_lbl->setText("PSoC Disconnected");
     ui->state_lbl->setStyleSheet("QLabel {font-weight: bold; color: red}");
 
-    /*
-     * density Slider
-     * this slider will let us control the
-     * brightness of the onboard LED of the kit.
-    */
+    // density Slider
+    // this slider will let us control the
+    // brightness of the onboard LED of the kit.
     ui->density_Slider->setMinimum(0);
     ui->density_Slider->setMaximum(255);
     ui->density_Slider->setTickPosition(QSlider::TicksBelow);
@@ -91,20 +79,26 @@ Widget::Widget(QWidget *parent) :
 Widget::~Widget()
 {
     delete ui;
-    /*
-     * Nos debemos asegurar de que nos
-     * desconectamos de libusb
-     */
+
+    // Nos debemos asegurar de que nos desconectamos de libusb
+
 }
 
 void Widget::connect2USB()
 {    
     if(this->isConnected == true){
-        /*
-         * Si ya estabamos conectados entonces el clic
-         * es para desconectarnos
-        */
+
+        // Si ya estabamos conectados entonces el clic
+        // es para desconectarnos
+        ui->density_Slider->setEnabled(false);
+        ui->state_lbl->setText("Disconnected");
+        ui->state_lbl->setStyleSheet("QLabel {font-weight: bold; color: red}");
+        ui->connect_pB->setText("Connect");
+        ui->connect_pB->setStyleSheet("QPushButton {font-weight: bold; color : green}");
+
         timer->stop();
+        qDebug() << "Timer Detenido";
+
         this->isConnected = false;
     }else{
         result = libusb_init(NULL);
@@ -115,12 +109,12 @@ void Widget::connect2USB()
         }
 
         ui->density_Slider->setEnabled(true);
-        timer->start(10);
         ui->state_lbl->setText("Connected");
         ui->state_lbl->setStyleSheet("QLabel {font-weight: bold; color: green}");
-
         ui->connect_pB->setText("Disconnect");
         ui->connect_pB->setStyleSheet("QPushButton {font-weight: bold; color : red}");
+
+        timer->start(10);
 
         this->isConnected = true;
     }
